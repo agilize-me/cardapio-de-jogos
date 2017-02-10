c33a50421cba338b6278ea7d839ee357c7e09dfc
mfstabile <mf_stabile@hotmail.com>
2017-02-10 16:28:42 -0200

    Added game list and to_json

--- src/cardapio_de_jogos/core.clj
+++ src/cardapio_de_jogos/core.clj
@@ -1,7 +1,29 @@
 (ns cardapio-de-jogos.core
   (:gen-class))
 
+(defn escape-string [x]
+  (clojure.string/replace x #"^[':\\]" "\\\\$0"))
+
+(defn code-to-json [x]
+  (condp #(%1 %2) x
+    number?  x
+    symbol?  (str \' (name x))
+    keyword? (str \: (name x))
+    string?  (escape-string x)
+    list?    (into [] (cons "list"   (map code-to-json x)))
+    vector?  (into [] (cons "vector" (map code-to-json x)))
+    set?     (into [] (cons "set"    (map code-to-json x)))
+    map?     (into {} (map #(mapv code-to-json %) x))
+    (throw (Exception. (format "Unsupported type: %s" (type x))))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (do (def gamelist [{:name "The Resistance"
+                      :min-player 5
+                      :max-player 10}
+                     {:name "Dixit"
+                      :min-player 3
+                      :max-player 12}])
+      (doseq [i gamelist] (println (get i :name)))
+      (println (code-to-json gamelist))))
