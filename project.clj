fbcae6d40d23ab6c2e16bbdde7d15c88296a547b
mfstabile <mf_stabile@hotmail.com>
2017-02-10 17:17:35 -0200

    Added Routes and response

--- project.clj
+++ project.clj
@@ -1,9 +1,14 @@
 (defproject cardapio-de-jogos "0.1.0-SNAPSHOT"
+  :plugins [[lein-ring "0.8.11"]]
+  :ring {:handler cardapio-de-jogos.core/handler}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [liberator "0.14.1"]
+                 [compojure "1.3.4"]
+                 [ring/ring-core "1.2.1"]]
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
   :main ^:skip-aot cardapio-de-jogos.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/cardapio_de_jogos/core.clj
+++ src/cardapio_de_jogos/core.clj
@@ -1,5 +1,7 @@
 (ns cardapio-de-jogos.core
-  (:gen-class))
+  (:require [liberator.core :refer [resource defresource]]
+            [ring.middleware.params :refer [wrap-params]]
+            [compojure.core :refer [defroutes ANY]]))
 
 (defn escape-string [x]
   (clojure.string/replace x #"^[':\\]" "\\\\$0"))
@@ -16,6 +18,33 @@
     map?     (into {} (map #(mapv code-to-json %) x))
     (throw (Exception. (format "Unsupported type: %s" (type x))))))
 
+(defn get-games [] (code-to-json [{:name "The Resistance"
+                                   :min-player 5
+                                   :max-player 10}
+                                  {:name "Dixit"
+                                   :min-player 3
+                                   :max-player 12}]))
+
+
+(defresource parameter [txt]
+             :available-media-types ["text/plain"]
+             :handle-ok (fn [_] (format "The text is %s" txt)))
+
+(defroutes app
+           (ANY "/foo" [] (resource :available-media-types ["text/html"]
+                                    :handle-ok (fn [ctx]
+                                                 (format "<html>It's %d milliseconds since the beginning of the epoch."
+                                                         (System/currentTimeMillis)))))
+           (ANY "/games" [] (resource :available-media-types ["text/html"]
+                                    :handle-ok (fn [ctx]
+                                                 (format "%s"
+                                                         (get-games)))))
+           (ANY "/bar/:txt" [txt] (parameter txt)))
+
+(def handler
+  (-> app
+      wrap-params))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
@@ -27,3 +56,7 @@
                       :max-player 12}])
       (doseq [i gamelist] (println (get i :name)))
       (println (code-to-json gamelist))))
+
+(defresource hello-world
+             :available-media-types ["text/plain"]
+             :handle-ok "Hello, world!")
